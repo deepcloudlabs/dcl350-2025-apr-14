00fd3c3eb309c4536b8717936bd9e12ac4940838
Binnur KURT <deepcloudlabs@gmail.com>
2025-04-15 14:03:03 +0300

    initial commit

--- hr-core-subdomain/src/com/example/hr/application/business/StandardHrApplication.java
+++ hr-core-subdomain/src/com/example/hr/application/business/StandardHrApplication.java
@@ -2,19 +2,25 @@ package com.example.hr.application.business;
 
 import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 import com.example.hr.application.HrApplication;
 import com.example.hr.domain.Employee;
 import com.example.hr.domain.TcKimlikNo;
+import com.example.hr.domain.event.EmployeeFiredEvent;
+import com.example.hr.domain.event.EmployeeHiredEvent;
 import com.example.hr.hexagonal.Application;
+import com.example.hr.infra.EventPublisher;
 import com.example.hr.repository.EmployeeRepository;
 
 @Application(api = HrApplication.class)
 public class StandardHrApplication implements HrApplication {
 	private final EmployeeRepository employeeRepository;
-
-	public StandardHrApplication(EmployeeRepository employeeRepository) {
+    private final EventPublisher eventPublisher;
+    
+	public StandardHrApplication(EmployeeRepository employeeRepository, EventPublisher eventPublisher) {
 		this.employeeRepository = employeeRepository;
+		this.eventPublisher = eventPublisher;
 	}
 
 	@Override
@@ -25,13 +31,18 @@ public class StandardHrApplication implements HrApplication {
 			throw new IllegalArgumentException(
 					"Cannot hire: Employee (%s) already exists.".formatted(identity.getValue()));
 		var persistedEmployee = employeeRepository.persist(employee);
+		var event = new EmployeeHiredEvent(persistedEmployee);
+		eventPublisher.publishEvent(event);
 		return persistedEmployee;
 	}
 
 	@Override
 	public Optional<Employee> fireEmployee(TcKimlikNo identity) {
 		var employee = employeeRepository.findById(identity);
-		employee.ifPresent(firedEmployee -> employeeRepository.remove(firedEmployee));
+		Consumer<Employee> fireEmployee = employeeRepository::remove;
+		Consumer<Employee> publishEvent = firedEmployee -> eventPublisher.publishEvent(new EmployeeFiredEvent(firedEmployee));
+		employee.ifPresent(fireEmployee.andThen(publishEvent));
+		
 		return employee;
 	}
 
