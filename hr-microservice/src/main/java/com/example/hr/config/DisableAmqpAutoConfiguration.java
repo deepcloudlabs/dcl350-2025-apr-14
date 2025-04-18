3ea8ce108cb3c2340331c09c8be8ea77f925b7c8
Binnur KURT <deepcloudlabs@gmail.com>
2025-04-18 10:49:24 +0300

    initial commit

--- hr-microservice/src/main/java/com/example/hr/config/DisableAmqpAutoConfiguration.java
+++ hr-microservice/src/main/java/com/example/hr/config/DisableAmqpAutoConfiguration.java
@@ -1,13 +1,13 @@
 package com.example.hr.config;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConditionalOnProperty(value = "messagingStrategy", havingValue = "kafka")
-@EnableAutoConfiguration(exclude = { KafkaAutoConfiguration.class })
+@EnableAutoConfiguration(exclude = { RabbitAutoConfiguration.class })
 public class DisableAmqpAutoConfiguration {
 
 }
