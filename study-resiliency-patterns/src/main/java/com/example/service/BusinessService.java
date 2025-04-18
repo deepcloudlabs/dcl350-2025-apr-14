7745d713f30e76ba58d4d1c4c78a40fd72d27681
Binnur KURT <deepcloudlabs@gmail.com>
2025-04-18 15:55:49 +0300

    initial commit

--- study-resiliency-patterns/src/main/java/com/example/service/BusinessService.java
+++ study-resiliency-patterns/src/main/java/com/example/service/BusinessService.java
@@ -8,14 +8,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class BusinessService {
 	private final IntegrationService integrationService;
-	
+
 	public BusinessService(IntegrationService integrationService) {
 		this.integrationService = integrationService;
 		System.out.println(this.integrationService.getClass());
 	}
 
-	@Scheduled(fixedRate = 10_000)
+	// @Scheduled(fixedRate = 10_000)
 	public void doBusiness() {
-		System.out.println("[%s] Received: %d".formatted(new Date(),integrationService.callRemoteService()));
+		System.out.println("[%s] Received: %d".formatted(new Date(), integrationService.callRemoteService()));
+	}
+
+	// @Scheduled(fixedRate = 100)
+	public void doBusiness2() {
+		System.out.println("[%s] Received: %d".formatted(new Date(), integrationService.callRemoteService2()));
+	}
+
+	@Scheduled(fixedRate = 10_000)
+	public void doBusiness3() {
+		integrationService.callRemoteService3().thenAcceptAsync(System.out::println);
 	}
 }
--- study-resiliency-patterns/src/main/java/com/example/service/IntegrationService.java
+++ study-resiliency-patterns/src/main/java/com/example/service/IntegrationService.java
@@ -1,10 +1,18 @@
 package com.example.service;
 
+import java.util.Date;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.stereotype.Service;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead.Type;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
 @Service
 public class IntegrationService {
@@ -18,7 +26,34 @@ public class IntegrationService {
 		return 42;
 	}
 	
+	@RateLimiter(name="integrationService2")
+	public int callRemoteService2() {
+		System.out.println("remoteService2: %s".formatted(new Date()));
+		return 549;
+	}
+	
+	@TimeLimiter(name="integrationService3",fallbackMethod = "fallbackForCallRemoteService3")
+	public CompletableFuture<Integer> callRemoteService3() {
+		return CompletableFuture.supplyAsync(()->{
+			try {TimeUnit.SECONDS.sleep(ThreadLocalRandom.current().nextInt(1, 6));} catch (InterruptedException e) {}
+			return 3615;
+		});
+	}
+	
+	
+	@CircuitBreaker(name="integrationService4")
+	@Bulkhead(name="integrationService4",type = Type.SEMAPHORE)
+	public int callRemoteService4() {
+		System.out.println("remoteService4: %s".formatted(new Date()));
+		return 549;
+	}
+	
 	public int fallbackForCallRemoteService(Throwable t) {
 		return 108;
 	}
+	
+	public CompletableFuture<Integer>  fallbackForCallRemoteService3(Throwable t) {
+		return CompletableFuture.supplyAsync(()->{
+			return 108;
+		});	}
 }
