a8a8007a6af2ff999b08bd1ddfc10e1a271fe209
Binnur KURT <deepcloudlabs@gmail.com>
2025-04-18 17:41:07 +0300

    Update AutoRefresher.java

--- lottery-microservice/src/main/java/com/example/lottery/service/AutoRefresher.java
+++ lottery-microservice/src/main/java/com/example/lottery/service/AutoRefresher.java
@@ -1,7 +1,7 @@
 package com.example.lottery.service;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
